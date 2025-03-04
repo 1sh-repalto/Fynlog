04db4e86c14cf5956858aae92f86c1e003b919d1
vansh <vanshrepalto@gmail.com>
2025-03-04 22:45:38 +0530

    database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,21 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import sequelize from "./config/db";
 
 dotenv.config();
 
 const app: Express = express();
+
+app.use(express.json());
 const PORT = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello");
-})
+});
+
+sequelize
+    .sync()
+    .then(() => console.log("Database connected successfully!"))
+    .catch((err) => console.error("Error connecting to database: ", err));
 
 app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
