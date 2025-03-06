777ebb5c377b73fc42650914ee60d587e4d95f6a
vansh <vanshrepalto@gmail.com>
2025-03-06 19:57:18 +0530

    defined models

---
+++ backend/src/models/category.ts
@@ -0,0 +1,55 @@
+import { DataTypes, Model, Optional } from "sequelize";
+import sequelize from "../config/db";
+import User from "./user";
+
+interface CategoryAttributes {
+  id: number;
+  userId: number;
+  name: string;
+}
+
+interface CategoryCreationAttributes
+  extends Optional<CategoryAttributes, "id"> {}
+
+class Category
+  extends Model<CategoryAttributes, CategoryCreationAttributes>
+  implements CategoryAttributes
+{
+  public id!: number;
+  public userId!: number;
+  public name!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Category.init(
+  {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    },
+    name: { type: DataTypes.STRING, allowNull: false },
+  },
+  {
+    sequelize,
+    tableName: "categories",
+    indexes: [
+      {
+        unique: true,
+        fields: ["userId", "name"],
+      },
+    ],
+  }
+);
+
+User.hasMany(Category, { foreignKey: "userId" });
+Category.belongsTo(User, { foreignKey: "userId" });
+
+export default Category;
---
+++ backend/src/models/transaction.ts
@@ -0,0 +1,79 @@
+import { DataTypes, Model, Optional } from "sequelize";
+import sequelize from "../config/db";
+import User from "./user";
+import Category from "./category";
+
+interface TransactionAttributes {
+  id: number;
+  userId: number;
+  categoryId: string;
+  amount: number;
+  type: "income" | "expense";
+  date: Date;
+  description?: string;
+}
+
+interface TransactionCreationAttributes
+  extends Optional<TransactionAttributes, "id"> {}
+
+class Transaction
+  extends Model<TransactionAttributes, TransactionCreationAttributes>
+  implements TransactionAttributes
+{
+  public id!: number;
+  public userId!: number;
+  public categoryId!: string;
+  public amount!: number;
+  public type!: "income" | "expense";
+  public date!: Date;
+  public description!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Transaction.init(
+  {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: User, key: "id" },
+      onDelete: "CASCADE",
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: Category, key: "id" },
+      onDelete: "SET NULL",
+    },
+    amount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.ENUM("income", "expense"),
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    tableName: "transactions",
+  }
+);
+
+User.hasMany(Transaction, { foreignKey: "userId" });
+Transaction.belongsTo(User, { foreignKey: "userId" });
+
+Category.hasMany(Transaction, { foreignKey: "categoryId" });
+Transaction.belongsTo(Category, { foreignKey: "categoryId" });
+
+export default Transaction;
