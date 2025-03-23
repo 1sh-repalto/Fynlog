42b8f821b83ff8d1034808f6a4af1bfceefe9a35
Vansh Negi <vanshrepalto@gmail.com>
2025-03-23 10:43:17 +0530

    added navigation to CTA button

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
 const LandingPage = () => {
+  const navigate = useNavigate();
   return (
     <div className="absolute inset-0 -z-10 h-full w-full bg-dark bg-[linear-gradient(to_right,#1f1f1f_1px,transparent_1px),linear-gradient(to_bottom,#1f1f1f_1px,transparent_1px)] bg-[size:4rem_6rem]">
       <div className="max-w-5xl mx-auto text-center mt-44">
@@ -12,7 +15,10 @@ const LandingPage = () => {
         <p className="italic text-xl text-neutral mt-6 font-light">
           Plan, save, and spend wisely with our easy-to-use expense tracker.
         </p>
-        <button className="mt-14 py-4 px-8 text-lg font-medium text-neutral bg-secondary rounded-4xl hover:drop-shadow-[0px_0px_8px_rgba(39,174,96,0.8)] active:drop-shadow-none active:bg-[#208049] transition duration-200">
+        <button
+          onClick={() => navigate("/auth")}
+          className="mt-14 py-4 px-8 text-lg font-medium text-neutral bg-secondary rounded-4xl hover:drop-shadow-[0px_0px_8px_rgba(39,174,96,0.8)] active:drop-shadow-none active:bg-[#208049] transition duration-200"
+        >
           Get Started Now!
         </button>
       </div>
