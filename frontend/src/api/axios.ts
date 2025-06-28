d903d8d682d588d6efb30e0eeca3bbcc73c3c48b
Vansh <vanshrepalto@gmail.com>
2025-06-29 00:12:55 +0530

    added use of .env variables in frontend for backend api route

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -3,7 +3,7 @@ import { logout } from './auth';
 import { handleApiErrors } from '../utils/handleApiErrors';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
